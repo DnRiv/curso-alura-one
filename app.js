3ac1eb7846c097a607865150b910d8749448c828
Daniel Rivera <daniel.rivera.vill@gmail.com>
2025-01-27 11:19:25 -0300

    Añade funcion mostrar lista de amigos

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ let amigos = [];
 
 const botonAñadir = document.querySelector('.button-add');
 const inputAmigo = document.getElementById('amigo');
+const listaAmigos = document.getElementById('listaAmigos');
 
 botonAñadir.addEventListener('click', e =>{
-    const nombre = inputAmigo.value;
+    const nombre = inputAmigo.value.trim();
     
     if(nombre ===''){
-        alert('Por favor, inserte un nombre.');
-    } return false;
+    alert('Por favor, inserte un nombre.');
+    return false };
 
     amigos.push(nombre);
     console.log(amigos);
-    nombre = "";
-});
+    renderLists()
+    inputAmigo.value = "";
+
+    }
+);
+
+function renderLists() {
+    const listaAmigosUl = document.getElementById('listaAmigos');
+    listaAmigosUl.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigosUl.appendChild(li);
+    });
+}
 
