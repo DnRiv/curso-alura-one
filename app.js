33e43b190e3a30c88671375f3b7719416b9c8948
Daniel Rivera <daniel.rivera.vill@gmail.com>
2025-01-27 10:53:36 -0300

    Añade funcion añadir amigo al presionar boton

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const botonAñadir = document.querySelector('.button-add');
+const inputAmigo = document.getElementById('amigo');
+
+botonAñadir.addEventListener('click', e =>{
+    const nombre = inputAmigo.value;
+    
+    if(nombre ===''){
+        alert('Por favor, inserte un nombre.');
+    } return false;
+
+    amigos.push(nombre);
+    console.log(amigos);
+    nombre = "";
+});
+
